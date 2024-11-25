b3041af6ab790608aaafcea6e38c779438d5d8b3
ValzerBR <breno.scalzer@hotmail.com>
2024-11-25 13:31:11 -0300

    corrigido excluir_produtos para apenas usuario que criou conseguir deletar

--- excluir_produto.php
+++ excluir_produto.php
@@ -26,10 +26,20 @@ if(autenticar($db_con)){
     if (isset($_POST['id'])){
         // deleta produto de id especificado
         $id = trim($_POST['id']);
-        $consulta = $db_con->prepare("DELETE FROM produtos WHERE (id='$id')");
-        $consulta->execute();
-        $resposta["sucesso"] = 1;
-        $resposta["erro"] = "produto excluído";
+        $login = $GLOBALS['login'];
+        $consulta_produto_existe = $db_con->prepare("SELECT nome FROM produtos WHERE (id='$id') AND (usuarios_login ='$login')");
+        $consulta_produto_existe->execute();
+        if ($consulta_produto_existe->rowCount() > 0) {
+            $consulta = $db_con->prepare("DELETE FROM produtos WHERE (usuarios_login = '$login') AND (id='$id')");
+            $consulta->execute();
+            $resposta["sucesso"] = 1;
+            $resposta["erro"] = "produto excluído";
+        }
+        else{
+            $resposta["sucesso"] = 0;
+            $resposta["erro"] = "usuario nao criou produto ou id nao encontrado";
+            $resposta["cod_erro"] = 3;
+        }
     }
     else{
         // faltam parametros
